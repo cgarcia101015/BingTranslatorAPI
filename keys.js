0a28270b61d22cfbbfcec92c6c74c34b3bfcefdb
Clos Devop <31907938+cgarcia101015@users.noreply.github.com>
2019-03-09 15:26:15 -0500

    created function to loop & translate multiple phrases

--- keys.js
+++
@@ -1,8 +0,0 @@
-// console.log('key loaded');
-// require('dotenv');
-
-// const APIKeys = {
-// 	subKey: process.env.TRANSLATOR_TEXT_KEY
-// };
-
-// module.exports = APIKeys;
--- translate-text.js
+++ translate-text.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const request = require('request');
 const uuidv4 = require('uuid/v4');
-// var keys = require('./keys.js');
-// console.log(APIKeys.subKey);
+
 /* Checks to see if the subscription key is available
 as an environment variable. If you are setting your subscription key as a
 string, then comment these lines out.
@@ -10,36 +9,42 @@ string, then comment these lines out.
 If you want to set your subscription key as a string, replace the value for
 the Ocp-Apim-Subscription-Key header as a string. */
 const subscriptionKey = process.env.TRANSLATOR_TEXT_KEY;
+let phrases = ['Hello', 'Good morning', 'Please', 'How much money',];
 
-console.log(subscriptionKey);
-if (!subscriptionKey) {
-	throw new Error('Environment variable for your subscription key is not set.');
-}
+function translate(language,phrases) {
+	for (i = 0; i <= phrases.length; i++) {
 
-let options = {
-	method: 'POST',
-	baseUrl: 'https://api.cognitive.microsofttranslator.com/',
-	url: 'translate',
-	qs: {
-		'api-version': '3.0',
-		// to: 'it',
-		// to: 'sv',
-		to: 'it'
-	},
-	headers: {
-		'Ocp-Apim-Subscription-Key': subscriptionKey,
-		'Content-type': 'application/json',
-		'X-ClientTraceId': uuidv4().toString()
-	},
-	body: [
-		{
-			text: 'Hello, where is the nearest train station?'
+		if (!subscriptionKey) {
+			throw new Error('Environment variable for your subscription key is not set.');
 		}
-	],
-	json: true
-};
 
-request(options, function(err, res, body) {
-	console.log(JSON.stringify(body, null, 4));
-	// console.log(JSON.stringify(body, null, 4));
-});
+		let options = {
+			method: 'POST',
+			baseUrl: 'https://api.cognitive.microsofttranslator.com/',
+			url: 'translate',
+			qs: {
+				'api-version': '3.0',
+				'to': language
+
+			},
+			headers: {
+				'Ocp-Apim-Subscription-Key': subscriptionKey,
+				'Content-type': 'application/json',
+				'X-ClientTraceId': uuidv4().toString()
+			},
+			body: [{
+				'text': phrases[i],
+			}],
+			json: true,
+		};
+		request(options, function (err, res, body) {
+			console.log(JSON.stringify(body, null, 5	));
+		});
+	}
+	
+}
+// translate("it",phrases);
+// translate("es", phrases);
+translate('es', phrases);
+
+
