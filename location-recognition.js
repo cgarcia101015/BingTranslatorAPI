9dfdf2c60737b6bda596db7b1476a57dab1d6a70
Clos Devop <31907938+cgarcia101015@users.noreply.github.com>
2019-03-23 13:07:23 -0400

    tranlsation based on ip address working

--- location-recognition.js
+++ location-recognition.js
@@ -6,31 +6,11 @@ var URL = "https://www.ipapi.co/";
 URL += "json";
 
 axios.get(URL).then(async response => {
-    const geoLoc = await recogLang(response.data.ip);
+    // const geoLoc = await recogLang(response.data.ip);
 
-    // const geoLoc = await recogLang("77.136.14.169");
-    console.log(geoLoc);
+    const geoLoc = await recogLang("77.136.14.169");
+    // console.log(geoLoc);
     translate(geoLoc.countryCode);
 
 });
 
-/*
-var info = request({
-    url: URL,
-    json: true
-}, (err, response, body) => {
-    if (!err && response.statusCode == 200) {
-        IPAddress = {
-            key: body.ip
-        };
-        // console.log(body);
-        recogLang(body.ip);
-    }
-});
-*/
-
-
-// console.log(IPAddress.body.ip);
-
-
-// module.exports= IPAddress;
