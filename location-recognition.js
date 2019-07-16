4269f783c3d09012837cad90038a99633e73c68b
Carlos M Garcia <cgarcia101015@gmail.com>
2019-07-15 22:57:32 -0400

    updated package

--- location-recognition.js
+++ location-recognition.js
@@ -8,14 +8,8 @@ URL += "json";
 axios.get(URL).then(async response => {
     // const geoLoc = await recogLang(response.data.ip);
 
-<<<<<<< HEAD
-    // const geoLoc = await recogLang("77.136.14.169");
-    console.log(geoLoc);
-=======
     const geoLoc = await recogLang("77.136.14.169");
     // console.log(geoLoc);
->>>>>>> e51bd74ba2fdd69f1b5353d01e23ade8e67f9fa0
     translate(geoLoc.countryCode);
 
 });
-
